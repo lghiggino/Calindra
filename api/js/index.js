61ce7084c13445653d514e851959fd3879468284
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-21 19:49:31 -0300

    fetch is done

--- api/js/index.js
+++ api/js/index.js
@@ -8,6 +8,12 @@ burgerIcon.addEventListener("click", () => {
 
 //listening for the "enter" or click, and fetching data
 const input = document.querySelector(".input")
+const searchButton = document.querySelector("#search-button")
+
+searchButton.addEventListener("click", () => {
+    let item = input.value
+    search(item)
+})
 
 window.addEventListener("keydown", (e) => {
     if (e.key === "Enter"){
@@ -20,36 +26,89 @@ async function search(item){
     try{
         const res = await fetch(`https://mystique-v2-americanas.juno.b2w.io/autocomplete?content="${item}"&source=nanook`)
         const data = await res.json()
-        render(data)
+        renderProducts(data)
+        renderSuggestions(data)
     } 
     catch(err){
         throw new Error(err)
     }
 }
 
-function render(data){
+function renderProducts(data){
     const cardContainer = document.querySelector("#card-container")
     cardContainer.innerHTML = ""
-    if(data.products.length === 0){cardContainer.innerText = "Nenhum produto encontrado. Tente buscar por outras palavras."}
+    if(data.products.length === 0){return cardContainer.innerText = "Nenhum produto encontrado. Tente buscar por outras palavras."}
     else{
-        console.log("renderizando os dados")
-        console.log(data.products, data.suggestions)
+        //below and above variables could be dismissed, are here for better readability of the ternary
+        let averageRating = renderScoreColors(data)
+        let belowAverageRating = averageRating * 0.75
+        let aboveAverageRating = averageRating * 1.25
+        let averageVisits = renderVisitColors(data)
+        let belowAverageVisits = averageVisits * 0.75
+        let aboveAverageVisits = averageVisits * 1.25
         data.products.map(el => {
-            // console.log(el.name, el.id, el["_meta"].score, el["_meta"].visitsClickCount)
+            let name = el.name
+            let id = el.id
+            let score = el["_meta"].score
+            let visits = +el["_meta"].visitsClickCount
             cardContainer.innerHTML += `
-            <div class="column   is-12-mobile is-6-tablet is-2-desktop">
+            <div class="column is-12-mobile is-6-tablet is-4-desktop">
                 <div class="card" style="min-height: 200px">
                     <div class="content">
-                        <p class="title is-5 ml-2">${el.name}</p>
-                        <p class="is-5 ml-2">Id: ${el.id}</p>
-                        <p class="is-5 ml-2">Score: ${el["_meta"].score}</p>
-                        <p class="is-5 ml-2 mb-3">Visits: ${el["_meta"].visitsClickCount}</p>
+                        <p class="title is-5 ml-2">${name}</p>
+                        <p class="is-5 ml-2">Id: ${id}</p>
+                        <p class="is-5 ml-2 ${score <= belowAverageRating ? 'has-text-danger' 
+                            : score >= aboveAverageRating ? 'has-text-success' 
+                                : 'has-text-black'}">Score: ${score} </p>
+                        <p class="is-5 ml-2 mb-3 ${visits <= belowAverageVisits ? 'has-text-danger' 
+                            : visits >= aboveAverageVisits ? 'has-text-success' 
+                                : 'has-text-black'}">Visits: ${visits}</p>
                     </div>
                 </div>
             </div>
             `
         })
-
+        cardContainer.innerHTML += `
+        <div class="column is-12">
+            <p class="subtitle is-size-6">Itens na cor verde estão 25% ou mais acima da média. Itens em vermelho estão 25% ou mais abaixo da média. Itens em preto estão no intervalo da média +- 25%</p>
+        </div>
+        `
     }
-    
+    //input.value = ""
+}
+
+function renderScoreColors(data){
+    let allScores = []
+    data.products.map(el => {
+        allScores.push(el["_meta"].score)
+    })
+    let average = allScores.reduce((acc,curr) => acc+curr,0)/6
+    return Number(average.toFixed(3));
+}
+
+function renderVisitColors(data){
+    let allVisits= []
+    data.products.map(el => {
+        allVisits.push(el["_meta"].visitsClickCount)
+    })
+    let visits = allVisits.reduce((acc,curr) => +acc + +curr,0)/6
+    return Number(visits.toFixed(3));
+}
+
+function renderSuggestions(data){
+    const suggTitle = document.querySelector("#sugg-title")
+    const suggContainer = document.querySelector("#sugg-container")
+    if(data.suggestions.length === 0){return suggestionTitle.classList.contains("is-hidden") ? "" : suggestionTitle.classList.add("is-hidden")}
+    else{
+        suggTitle.classList.remove("is-hidden")
+        suggContainer.innerHTML = ""
+        data.suggestions.map(el => {
+            console.log(el.term)
+            suggContainer.innerHTML += `
+            <div class="column is-12-mobile is-6-tablet is-4-desktop">
+                <a href="/"><span class="tag is-light is-light is-medium">${el.term}</span></a>
+            </div>
+            `
+        })
+    }
 }
