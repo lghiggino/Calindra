74d7b4177abd126aa459c2e02ba533c3e0018c99
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-23 09:55:50 -0300

    clearing styles

--- layout/zutterman/components/Header.js
+++ layout/zutterman/components/Header.js
@@ -5,37 +5,17 @@ import Nav from "./Nav"
 
 export default function Header(){
     return (
-        <header>
-            {/* <nav>
-                <div className="flex ml-2">
-                    <div className="dots is-hidden-desktop"> <HiDotsVertical /></div>
-                        <Link href="/">
-                            <a class="logo-container">
-                                <img className="logo" src="./images/logo-Z.webp"alt="zuterman logo"/>
-                                <div className="text-container ml-4">
-                                    <div><h1 className="lower-case is-hidden-mobile">zutterman</h1></div>
-                                    <div><span className="is-hidden-mobile">Measure Tapes</span></div>
-                                </div>
-                            </a>
-                        </Link>
-                </div>
-                <div className="flex column mr-2">
-                    <Link href="/guarantee"><a className="is-hidden-mobile .mr-2 link" >guarantee</a></Link>
-                    <Link href="/about-us"><a className="is-hidden-mobile .mr-2 link" >about us</a></Link>
-                    <Link href="/models"><a className="is-hidden-mobile .mr-2 link" >models</a></Link>
-                </div>
-            </nav> */}
-            
+        <header>         
             <div className="flex">
-                <Nav></Nav>
+                <Nav className="is-hidden-desktop"></Nav>
                 <Link href="/">
-                    <a class="logo-container flex">
-                        <img className="logo" src="./images/logo-Z.webp"alt="zuterman logo"/>
+                    <a className="logo-container flex">
+                        <img className="logo" src="./images/logo.svg"alt="zuterman logo"/>
                     </a>
                 </Link>
                 <div className="text-container ml-4 is-hidden-mobile">
-                    <div><h1 className="lower-case ">zutterman</h1></div>
-                    <div><span className="">Measure Tapes</span></div>
+                    <div><h1>zutterman</h1></div>
+                    <div><span>Measure Tapes</span></div>
                 </div>
                 <nav className="flex links-container column is-hidden-mobile mr-2">
                     <Link href="/guarantee"><a className="mr-2 link" >guarantee</a></Link>
@@ -43,7 +23,6 @@ export default function Header(){
                     <Link href="/models"><a className="mr-2 link" >models</a></Link>
                 </nav>
             </div>
-            
         </header>
     )
 }
