17bb55b2b1829e75e08209d456982eb860d5ca51
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-24 12:07:31 -0300

    history object is carrying the data, no fetch

--- api/js/index.js
+++ api/js/index.js
@@ -1,71 +1,70 @@
 // menu mobile
-const burgerIcon = document.querySelector(".navbar-burger")
-const navbarMenu = document.querySelector("#nav-links")
+const burgerIcon = document.querySelector(".navbar-burger");
+const navbarMenu = document.querySelector("#nav-links");
 
 burgerIcon.addEventListener("click", () => {
-    navbarMenu.classList.toggle("is-active")
+    navbarMenu.classList.toggle("is-active");
 })
 
 //listening for the "enter" or click, and fetching data
-const input = document.querySelector(".input")
-const searchButton = document.querySelector("#search-button")
+const input = document.querySelector(".input");
+const searchButton = document.querySelector("#search-button");
 
 searchButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    let item = input.value
-    search(item)
+    e.preventDefault();
+    let item = input.value;
+    search(item);
 })
 
 window.addEventListener("keydown", (e) => {
     if (e.key === "Enter"){
-        e.preventDefault()
-        let item = input.value
-        search(item)
+        e.preventDefault();
+        let item = input.value;
+        search(item);
     }
 })
 
 async function search(item){
     try{
-        const res = await fetch(`https://mystique-v2-americanas.juno.b2w.io/autocomplete?content="${item}"&source=nanook`)
-        const data = await res.json()
-        renderProducts(data)
-        renderSuggestions(data)
-        pushIntoHistory(item)
+        const res = await fetch(`https://mystique-v2-americanas.juno.b2w.io/autocomplete?content="${item}"&source=nanook`);
+        const data = await res.json();
+        renderProducts(data);
+        renderSuggestions(data);
+        pushIntoHistory(item, data);
     } 
     catch(err){
-        throw new Error(err)
+        throw new Error(err);
     }
 }
 
 function renderProducts(data){
-    const cardContainer = document.querySelector("#card-container")
-    cardContainer.innerHTML = ""
-    if(data.products.length === 0){return cardContainer.innerText = "Nenhum produto encontrado. Tente buscar por outras palavras."}
+    const cardContainer = document.querySelector("#card-container");
+    cardContainer.innerHTML = "";
+    if(data === null){return cardContainer.innerText = "Olá! Digite o produto desejado, click ou aperte enter, e compare os resultados."}
+    else if(data.products.length === 0){return cardContainer.innerText = "Nenhum produto encontrado. Tente buscar por outras palavras."}
     else{
         //variables prepended with below and above could be dismissed, are here for better readability of the ternary
-        let averageRating = renderScoreColors(data)
-        let belowAverageRating = averageRating * 0.75
-        let aboveAverageRating = averageRating * 1.25
-        let averageVisits = renderVisitColors(data)
-        let belowAverageVisits = averageVisits * 0.75
-        let aboveAverageVisits = averageVisits * 1.25
+        const averageRating = generateScoreAverage(data);
+        const belowAverageRating = averageRating * 0.75;
+        const aboveAverageRating = averageRating * 1.25;
+        const averageVisits = generateVisitAverage(data);
+        const belowAverageVisits = averageVisits * 0.75;
+        const aboveAverageVisits = averageVisits * 1.25;
         data.products.map(el => {
-            let name = el.name
-            let id = el.id
-            let score = el["_meta"].score
-            let visits = +el["_meta"].visitsClickCount
+            const name = el.name;
+            const id = el.id;
+            const score = el["_meta"].score;
+            const visits = +el["_meta"].visitsClickCount;
             cardContainer.innerHTML += `
             <div class="column is-12-mobile is-6-tablet is-4-desktop">
                 <div class="card" style="min-height: 200px">
                     <div class="content">
                         <p class="title is-5 ml-2">${name}</p>
                         <p class="is-5 ml-2">Id: ${id}</p>
-                        <p class="is-5 ml-2 ${score <= belowAverageRating ? 'has-text-danger' 
-                            : score >= aboveAverageRating ? 'has-text-success' 
-                                : 'has-text-black'}">Score: ${score} </p>
-                        <p class="is-5 ml-2 mb-3 ${visits <= belowAverageVisits ? 'has-text-danger' 
-                            : visits >= aboveAverageVisits ? 'has-text-success' 
-                                : 'has-text-black'}">Visits: ${visits}</p>
+                        <p class="is-5 ml-2 ${score <= belowAverageRating ? 'has-text-danger' :
+                            score >= aboveAverageRating ? 'has-text-success' : 'has-text-black'}">Score: ${score} </p>
+                        <p class="is-5 ml-2 mb-3 ${visits <= belowAverageVisits ? 'has-text-danger' :
+                            visits >= aboveAverageVisits ? 'has-text-success' : 'has-text-black'}">Visits: ${visits}</p>
                     </div>
                 </div>
             </div>
@@ -79,35 +78,29 @@ function renderProducts(data){
     }
 }
 
-function renderScoreColors(data){
-    let allScores = []
-    data.products.map(el => {
-        allScores.push(el["_meta"].score)
-    })
-    let average = allScores.reduce((acc,curr) => acc+curr,0)/6
-    return Number(average.toFixed(3));
+function generateScoreAverage(data){
+    let allScores = data.products.map(el => el["_meta"].score);
+    let averageScore = allScores.reduce((acc,curr) => acc+curr,0)/6;
+    return Number(averageScore.toFixed(4));
 }
 
-function renderVisitColors(data){
-    let allVisits= []
-    data.products.map(el => {
-        allVisits.push(el["_meta"].visitsClickCount)
-    })
-    let visits = allVisits.reduce((acc,curr) => +acc + +curr,0)/6
-    return Number(visits.toFixed(3));
+function generateVisitAverage(data){
+    let allVisits = data.products.map(el => el["_meta"].visitsClickCount);
+    let averageVisits = allVisits.reduce((acc,curr) => +acc + +curr,0)/6;
+    return Number(averageVisits.toFixed(4));
 }
 
 function renderSuggestions(data){
-    const suggTitle = document.querySelector("#sugg-title")
-    const suggContainer = document.querySelector("#sugg-container")
+    const suggTitle = document.querySelector("#sugg-title");
+    const suggContainer = document.querySelector("#sugg-container");
     if(data.suggestions.length === 0){
-        suggestionTitle.classList.contains("is-hidden") ? "" : suggestionTitle.classList.add("is-hidden")
-        suggContainer.innerHTML = ""
+        suggestionTitle.classList.contains("is-hidden") ? "" : suggestionTitle.classList.add("is-hidden");
+        suggContainer.innerHTML = "";
         return
     }
     else{
-        suggTitle.classList.remove("is-hidden")
-        suggContainer.innerHTML = ""
+        suggTitle.classList.remove("is-hidden");
+        suggContainer.innerHTML = "";
         data.suggestions.map(el => {
             // console.log(el.term)
             suggContainer.innerHTML += `
@@ -117,37 +110,38 @@ function renderSuggestions(data){
             `
         })
     }
-    generateTagLinks()
+    generateTagLinks();
 }
 
 function generateTagLinks(){
-    const allTags = document.querySelectorAll(".tag")
+    const allTags = document.querySelectorAll(".tag");
     allTags.forEach(tag => {
         tag.addEventListener("click", (e) => {
-            search(e.target.innerText)
-            input.value = e.target.innerText
+            search(e.target.innerText);
+            input.value = e.target.innerText;
         })
     })
 }
 
 //handling the history API
-function pushIntoHistory(item){
-    history.pushState(item, null, ``) 
+function pushIntoHistory(item,data){
+    //store the data in the history object, and then just re-render it, avoiding a new fetch
+    history.pushState({id:item, data:data}, null, "");
 }
 
 window.addEventListener("popstate", (e) => {
     if (e.state !== null){
-        search(e.state)
-        input.value = e.state
-    }else{
-        location.href ="./"
-        input.value = ""
+        renderProducts(e.state.data)
+        renderSuggestions(e.state.data)
+        //search(e.state.id);
+        input.value = e.state.id;
     }
-    
+    else{
+        renderProducts(null)
+        input.value = "";
+    }
 })
 
 
-
-
 //to initialize the page with a few hardcoded working suggestions
-generateTagLinks()
+generateTagLinks();
